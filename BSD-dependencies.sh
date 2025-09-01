b423717121a56d95ca69e19a604524a8a57a8394
Escary <81011361+pflyly@users.noreply.github.com>
2025-09-01 20:05:10 +0100

    deps change 3

--- BSD-dependencies.sh
+++ BSD-dependencies.sh
@@ -5,11 +5,12 @@ case "$OS" in
   FreeBSD)
     sed -i '' -e 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
     export ASSUME_ALWAYS_YES=true
-    pkg install -y autoconf bash boost-libs catch2 cmake ccache gcc gmake git glslang libfmt libzip liblz4 lzlib mbedtls3 nasm \
+    pkg install -y autoconf bash boost-libs catch2 cmake ccache gcc gmake git glslang libfmt libzip nasm \
                 ninja pkgconf qt6-base qt6ct qt6-tools qt6-translations qt6-wayland sdl2 unzip vulkan-tools vulkan-loader wget zip
     ;;
   Solaris)
-    pkg update && sudo pkg install git cmake developer/gcc-14
+    pkg update 
+    pkg install git cmake developer/gcc-14
     pkg install qt6 libzip openssl zlib compress/zstd unzip pkg-config nasm autoconf mesa library/libdrm
     ;;
   *)
